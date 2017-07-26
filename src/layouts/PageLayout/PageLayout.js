ed06bf1e9b9a3ba681950039c0a228d0f3a93a9c
Ben Ebert <ben.ebert@corp.vegas.com>
2017-07-25 22:55:33 -0700

    Added react-i13n to test instrumentation of clicks

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
+import { createI13nNode } from 'react-i13n'
+
+const I13nIndexLink = createI13nNode(IndexLink, {
+  isLeafNode: true,
+  bindClickEvent: true,
+  follow: true
+})
+
+const I13nLink = createI13nNode(Link, {
+  isLeafNode: true,
+  bindClickEvent: true,
+  follow: true
+})
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
     <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+    <I13nIndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</I13nIndexLink>
     {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    <I13nLink to='/counter' activeClassName='page-layout__nav-item--active'>Counter</I13nLink>
     <div className='page-layout__viewport'>
       {children}
     </div>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import './styles/main.scss'
+import { setupI13n } from 'react-i13n'
 
 // Store Initialization
 // ------------------------------------
@@ -11,12 +12,29 @@ const store = createStore(window.__INITIAL_STATE__)
 // ------------------------------------
 const MOUNT_NODE = document.getElementById('root')
 
+const testPlugin = {
+  name: 'Test Plugin',
+  eventHandlers: {
+    click (payload, callback) {
+      console.log('Click with react-i13n!', payload)
+    },
+    enterViewport (payload, callback) {
+      console.log('Enter viewport with react-i13n!', payload)
+    }
+  }
+}
+
 let render = () => {
   const App = require('./components/App').default
+  const I13nApp = setupI13n(App, {
+    rootModelData: { site: 'test-app-router' },
+    isViewportEnabled: true,
+    handlerTimeout: 500
+  }, [testPlugin])
   const routes = require('./routes/index').default(store)
 
   ReactDOM.render(
-    <App store={store} routes={routes} />,
+    <I13nApp store={store} routes={routes} />,
     MOUNT_NODE
   )
 }
